06f809c8ca008694ab2a5b48b04ee8bca1ab1a4a
John123412341 <johnsavatta@gmail.com>
2022-10-13 18:37:55 -0400

    updated testfile and testresults

--- TestFile.py
+++ TestFile.py
@@ -1,3 +1,4 @@
+import unittest
 import datetime
 from datetime import datetime as dt
 from prettytable import PrettyTable
@@ -8,7 +9,7 @@ fam_tags = {"MARR":"Married","DIV":"Divorced","HUSB":"Husband ID","WIFE":"Wife I
 months = ["JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC"]
 
 # Open gedcom file
-gedcom_filename = "TEST.ged"
+gedcom_filename = "sprint2test.ged"
 with open(gedcom_filename, 'r') as fp:
     gedcom_data = fp.readlines()
 
@@ -266,7 +267,268 @@ for fam in fams:
           (full_today.month == months.index(divorced_date[1])+1 and full_today.day < int(divorced_date[0]))))):
       output += ("ERROR: FAMILY: US01: " + fam["ID"] + ": Divorce date, " + fam["Divorced"] + " is after the current date\n")
 
+#Giovanni Sprint 2
+
+# check if birth is before marriage of parents
+for fam in fams:
+    husb_id = fam["Husband ID"]
+    wife_id = fam["Wife ID"]
+    child_id = fam["Children"]
+    for indiv in indivs:
+        if indiv["ID"] in child_id:
+          full_child_bday = indiv["Birthday"].split()
+          full_mday = fam["Married"].split()
+          if (int(full_mday[2]) > int(full_child_bday[2]) and
+             (months.index(full_mday[1])+1 > months.index(full_child_bday[1])+1 and 
+             int(full_mday[0]) > int(full_child_bday[0]))):
+              output += ("ERROR: INDIVIDUAL: US08: " + indiv["ID"] + ": Parents marriage date, " + fam["Married"] + " occurs after birth of child, " + indiv["Birthday"] + "\n")
+            
+#check if birth is after death of parents
+for fam in fams:
+    husb_id = fam["Husband ID"]
+    wife_id = fam["Wife ID"]
+    child_id = fam["Children"]
+    child_bdays = []
+    dad_dday = []
+    wife_dday = []
+    for indiv in indivs:
+        if indiv["ID"] in child_id:
+            child_bdays.append(indiv["Birthday"].split())
+        if husb_id == indiv["ID"]:
+            dad_dday = indiv["Death"].split()
+        if wife_id == indiv["ID"]:
+            wife_dday = indiv["Death"].split()
+    for child_bday in child_bdays:
+      if (len(dad_dday) != 1 and
+        int(dad_dday[2]) < int(child_bday[2]) and
+        (months.index(dad_dday[1])+1 < months.index(child_bday[1])+1 and 
+        int(dad_dday[0]) < int(child_bday[0]))):
+        output += ("ERROR: INDIVIDUAL: US09: " + indiv["ID"] + ": Fathers Death date, " + dad_dday + " occurs before birth of child, " + child_bdays + "\n")
+      if (len(wife_dday) != 1 and
+        int(wife_dday[2]) < int(child_bday[2]) and
+        (months.index(wife_dday[1])+1 < months.index(child_bday[1])+1 and 
+        int(wife_dday[0]) < int(child_bday[0]))):
+        output += ("ERROR: INDIVIDUAL: US09: " + indiv["ID"] + ": Mothers Death date, " + wife_dday + " occurs before birth of child, " + child_bdays + "\n")
+            
+            
+# Maris Sprint 2
+
+# Check if multiple births are <5
+for fam in fams:
+    birthdays = []
+    for indiv in indivs:
+        if indiv['ID'] in fam['Children']:
+            birthdays.append(indiv['Birthday'])
+    childrenAtOnce = []
+    for birthday in birthdays:
+        childrenAtOnce.append(birthdays.count(birthday))
+    for i in childrenAtOnce:
+        if i >= 5:
+            output += ("ERROR: FAMILY: US14: " + fam["ID"] + ": Multiple Births >= 5 \n")
+        break
+
+# Check if families have fewer than 15 siblings
+for fam in fams:
+    numChildren = 0
+    for child in fam['Children']:
+        numChildren += 1
+    if numChildren > 14:
+        output += ("ERROR: FAMILY: US15: " + fam["ID"] + ": Number of siblings Not fewer than 15 \n")
+    
+    
+    
+# Anton Sprint 2
+
+# Check if all marriages occur at least 14 years after birth of both spouses
+for fam in fams:
+  married_date = fam["Married"].split()
+  husb_id = fam["Husband ID"]
+  wife_id = fam["Wife ID"]
+  for indiv in indivs:
+    if indiv["ID"] == husb_id or indiv["ID"] == wife_id:
+      full_bday = indiv["Birthday"].split()
+      if (int(full_bday[2])+14 > int(married_date[2]) or
+          (int(full_bday[2])+14 == int(married_date[2]) and 
+           (months.index(full_bday[1])+1 > months.index(married_date[1])+1 or 
+            (months.index(full_bday[1])+1 == months.index(married_date[1])+1 and int(full_bday[0]) > int(married_date[0]))))):
+        output += ("ERROR: FAMILY: US10: " + fam["ID"] + ": Marriage date, " + fam["Married"] + " not at least 14 years after birth of both spouses\n")
+
+# Check that no marriage should occur during marriage to another
+for fam in fams:
+  married_date = fam["Married"].split()
+  husb_id = fam["Husband ID"]
+  wife_id = fam["Wife ID"]
+  divorce_date = "N/A"
+  if fam["Divorced"] == "N/A":
+    for indiv in indivs:
+      if ((indiv["ID"] == husb_id or indiv["ID"] == wife_id) and
+          indiv["Death"] != "N/A"):
+        if divorce_date == "N/A":
+          divorce_date = indiv["Death"].split()
+        elif (int(indiv["Death"].split()[2]) < int(divorce_date[2]) or
+              (int(indiv["Death"].split()[2]) == int(divorce_date[2]) and 
+               (months.index(indiv["Death"].split()[1])+1 < months.index(divorce_date[1])+1 or 
+                (months.index(indiv["Death"].split()[1])+1 == months.index(divorce_date[1])+1 and int(indiv["Death"].split()[0]) < int(divorce_date[0]))))):
+            divorce_date = indiv["Death"].split()
+    if divorce_date == "N/A":
+      divorce_date = [full_today.day, months[full_today.month-1], full_today.year]
+  else:
+    divorce_date = fam["Divorced"].split()
+  for other_fam in fams:
+    if other_fam["ID"] != fam["ID"] and (husb_id == other_fam["Husband ID"] or wife_id == other_fam["Wife ID"]):
+      other_married_date = other_fam["Married"].split()
+      if ((int(married_date[2]) < int(other_married_date[2]) or
+           (int(married_date[2]) == int(other_married_date[2]) and 
+            (months.index(married_date[1])+1 < months.index(other_married_date[1])+1 or 
+             (months.index(married_date[1])+1 == months.index(other_married_date[1])+1 and int(married_date[0]) < int(other_married_date[0]))))) and
+          (int(other_married_date[2]) < int(divorce_date[2]) or
+           (int(other_married_date[2]) == int(divorce_date[2]) and 
+            (months.index(other_married_date[1])+1 < months.index(divorce_date[1])+1 or 
+             (months.index(other_married_date[1])+1 == months.index(divorce_date[1])+1 and int(other_married_date[0]) < int(divorce_date[0])))))):
+        output += ("ERROR: FAMILY: US11: " + other_fam["ID"] + ": Marriage date, " + other_fam["Married"] + " occurs within marriage of " + fam["ID"] + "\n")
+        
+        
+class testUserStory(unittest.TestCase):
+    # check if birth is before marriage of parents
+    def test_trueUS08(self): 
+      for fam in fams:
+        husb_id = fam["Husband ID"]
+        wife_id = fam["Wife ID"]
+        child_id = fam["Children"]
+        for indiv in indivs:
+            message = "Marriage of parents must be before birth of child!"
+            testValue = True
+            if indiv["ID"] in child_id:
+                full_child_bday = indiv["Birthday"].split()
+                full_mday = fam["Married"].split()
+                if (int(full_mday[2]) > int(full_child_bday[2]) and
+               (months.index(full_mday[1])+1 > months.index(full_child_bday[1])+1 and 
+                int(full_mday[0]) > int(full_child_bday[0]))):
+                    testValue = False
+        self.assertTrue(testValue, message) 
+
+    #check if birth is after death of parents 
+    def test_trueUS09(self):
+        message = "Birth must be before the death of parents"
+        testValue = True
+        for fam in fams:
+          husb_id = fam["Husband ID"]
+          wife_id = fam["Wife ID"]
+          child_id = fam["Children"]
+          child_bdays = []
+          dad_dday = []
+          wife_dday = []
+          for indiv in indivs:
+              if indiv["ID"] in child_id:
+                  child_bdays.append(indiv["Birthday"].split())
+              if husb_id == indiv["ID"]:
+                  dad_dday = indiv["Death"].split()
+              if wife_id == indiv["ID"]:
+                  wife_dday = indiv["Death"].split()
+          for child_bday in child_bdays:
+            if (len(dad_dday) != 1 and
+              int(dad_dday[2]) < int(child_bday[2]) and
+              (months.index(dad_dday[1])+1 < months.index(child_bday[1])+1 and 
+              int(dad_dday[0]) < int(child_bday[0]))):
+              testValue = False
+            if (len(wife_dday) != 1 and
+              int(wife_dday[2]) < int(child_bday[2]) and
+              (months.index(wife_dday[1])+1 < months.index(child_bday[1])+1 and 
+              int(wife_dday[0]) < int(child_bday[0]))):
+              testValue = False
+        self.assertTrue(testValue, message)
+        
+    # check if multiple births >5
+    def test_trueUS14(self):
+        message = "Multiples births should not have more than five children"
+        testValue = True
+        for fam in fams:
+            birthdays = []
+            for indiv in indivs:
+                if indiv['ID'] in fam['Children']:
+                    birthdays.append(indiv['Birthday'])
+                childrenAtOnce = []
+        for birthday in birthdays:
+            childrenAtOnce.append(birthdays.count(birthday))
+        for i in childrenAtOnce:
+            if i >= 5:
+                testValue = False
+            break
+        self.assertTrue(testValue, message)
+            
+    # check if count for number of children exists
+    def test_trueUS15(self):
+        message = "Families should not have more than fifteen children"
+        testValue = True
+        for fam in fams:
+            numChildren = 0
+            for child in fam['Children']:
+                numChildren += 1
+            if numChildren > 14:
+                testValue = False 
+        self.assertTrue(testValue, message) # check if multiple births >5 
+
+    # Check if all marriages occur at least 14 years after birth of both spouses
+    def test_trueUS10(self):
+      message = "All marriages must occur at least 14 years after birth of both spouses"
+      testValue = True
+      for fam in fams:
+        married_date = fam["Married"].split()
+        husb_id = fam["Husband ID"]
+        wife_id = fam["Wife ID"]
+        for indiv in indivs:
+          if indiv["ID"] == husb_id or indiv["ID"] == wife_id:
+            full_bday = indiv["Birthday"].split()
+            if (int(full_bday[2])+14 > int(married_date[2]) or
+                (int(full_bday[2])+14 == int(married_date[2]) and 
+                (months.index(full_bday[1])+1 > months.index(married_date[1])+1 or 
+                  (months.index(full_bday[1])+1 == months.index(married_date[1])+1 and int(full_bday[0]) > int(married_date[0]))))):
+              testValue = False
+      self.assertTrue(testValue, message)
+
+    # Check that no marriage should occur during marriage to another
+    def test_trueUS11(self):
+      message = "Marriage date must not occur during marriage to another"
+      testValue = True
+      for fam in fams:
+        married_date = fam["Married"].split()
+        husb_id = fam["Husband ID"]
+        wife_id = fam["Wife ID"]
+        divorce_date = "N/A"
+        if fam["Divorced"] == "N/A":
+          for indiv in indivs:
+            if ((indiv["ID"] == husb_id or indiv["ID"] == wife_id) and
+                indiv["Death"] != "N/A"):
+              if divorce_date == "N/A":
+                divorce_date = indiv["Death"].split()
+              elif (int(indiv["Death"].split()[2]) < int(divorce_date[2]) or
+                    (int(indiv["Death"].split()[2]) == int(divorce_date[2]) and 
+                    (months.index(indiv["Death"].split()[1])+1 < months.index(divorce_date[1])+1 or 
+                      (months.index(indiv["Death"].split()[1])+1 == months.index(divorce_date[1])+1 and int(indiv["Death"].split()[0]) < int(divorce_date[0]))))):
+                  divorce_date = indiv["Death"].split()
+          if divorce_date == "N/A":
+            divorce_date = [full_today.day, months[full_today.month-1], full_today.year]
+        else:
+          divorce_date = fam["Divorced"].split()
+        for other_fam in fams:
+          if other_fam["ID"] != fam["ID"] and (husb_id == other_fam["Husband ID"] or wife_id == other_fam["Wife ID"]):
+            other_married_date = other_fam["Married"].split()
+            if ((int(married_date[2]) < int(other_married_date[2]) or
+                (int(married_date[2]) == int(other_married_date[2]) and 
+                  (months.index(married_date[1])+1 < months.index(other_married_date[1])+1 or 
+                  (months.index(married_date[1])+1 == months.index(other_married_date[1])+1 and int(married_date[0]) < int(other_married_date[0]))))) and
+                (int(other_married_date[2]) < int(divorce_date[2]) or
+                (int(other_married_date[2]) == int(divorce_date[2]) and 
+                  (months.index(other_married_date[1])+1 < months.index(divorce_date[1])+1 or 
+                  (months.index(other_married_date[1])+1 == months.index(divorce_date[1])+1 and int(other_married_date[0]) < int(divorce_date[0])))))):
+              testValue = False
+        self.assertTrue(testValue, message)
+  
+
 # Open output file
 output_filename = "testresults.txt"
 with open(output_filename, 'w') as fp:
     fp.write(output)
+
+if __name__ == '__main__':
+  unittest.main()
