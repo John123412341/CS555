f8ac4f072d163c9b97db44a07684ab07bfabf9c6
Anton D <adanylenko29@gmail.com>
2022-10-13 15:04:05 -0400

    s

--- gedcom_parse.py
+++ gedcom_parse.py
@@ -276,12 +276,12 @@ for fam in fams:
     child_id = fam["Children"]
     for indiv in indivs:
         if indiv["ID"] in child_id:
-            full_child_bday = indiv["Birthday"].split()
-            full_mday = fam["Married"].split()
-            if (int(full_mday[2]) > int(full_child_bday[2]) and
-           (months.index(full_mday[1])+1 > months.index(full_child_bday[1])+1 and 
-            int(full_mday[0]) > int(full_child_bday[0]))):
-                output += ("ERROR: INDIVIDUAL: US08: " + indiv["ID"] + ": Parents marriage date, " + fam["Married"] + " occurs after birth of child, " + indiv["Birthday"] + "\n")
+          full_child_bday = indiv["Birthday"].split()
+          full_mday = fam["Married"].split()
+          if (int(full_mday[2]) > int(full_child_bday[2]) and
+             (months.index(full_mday[1])+1 > months.index(full_child_bday[1])+1 and 
+             int(full_mday[0]) > int(full_child_bday[0]))):
+              output += ("ERROR: INDIVIDUAL: US08: " + indiv["ID"] + ": Parents marriage date, " + fam["Married"] + " occurs after birth of child, " + indiv["Birthday"] + "\n")
             
 #check if birth is after death of parents
 for fam in fams:
@@ -295,15 +295,17 @@ for fam in fams:
         if indiv["ID"] in child_id:
             child_bdays += indiv["Birthday"].split()
         if husb_id == indiv["ID"]:
-            dad_dday += indiv["Death"].split()
+            dad_dday = indiv["Death"].split()
         if wife_id == indiv["ID"]:
-            wife_dday += indiv["Death"].split()
+            wife_dday = indiv["Death"].split()
     for child_bday in child_bdays:
-      if (int(dad_dday[2]) < int(child_bday[2]) and
+      if (len(dad_dday) != 1 and
+        int(dad_dday[0][2]) < int(child_bday[2]) and
         (months.index(dad_dday[1])+1 < months.index(child_bday[1])+1 and 
         int(dad_dday[0]) < int(child_bday[0]))):
         output += ("ERROR: INDIVIDUAL: US09: " + indiv["ID"] + ": Fathers Death date, " + dad_dday + " occurs before birth of child, " + child_bdays + "\n")
-      if (int(wife_dday[2]) < int(child_bday[2]) and
+      if (len(wife_dday) != 1 and
+        int(wife_dday[2]) < int(child_bday[2]) and
         (months.index(wife_dday[1])+1 < months.index(child_bday[1])+1 and 
         int(wife_dday[0]) < int(child_bday[0]))):
         output += ("ERROR: INDIVIDUAL: US09: " + indiv["ID"] + ": Mothers Death date, " + wife_dday + " occurs before birth of child, " + child_bdays + "\n")
